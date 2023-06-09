f889de0105c3153f0b1a8c05899c70f75e6cda4d
sermartinez <sermartinez@host.docker.internal>
2023-06-09 12:13:17 -0600

    logbook

--- src/main/java/com/gentera/domain/ServiceApplication.java
+++ src/main/java/com/gentera/domain/ServiceApplication.java
@@ -3,6 +3,7 @@ package com.gentera.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 
 @EnableAutoConfiguration
@@ -11,6 +12,7 @@ public class ServiceApplication {
 
     public static void main(String[] args) {
     	SpringApplication.run(ServiceApplication.class, args);
+    	Hooks.enableAutomaticContextPropagation();
     }
 
 }
